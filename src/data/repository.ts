7714a82220185f7a9eaee1cbdfc8854952dce3bc
nico <niccolo.rag@gmail.com>
2026-09-12 00:20:02 +0200

    hardening: reconcile orphaned media at startup

--- src/data/repository.ts
+++ src/data/repository.ts
@@ -1,4 +1,5 @@
 import { initializeDatabase } from './database';
+import { reconcileOwnedMedia } from './media';
 import { SQLiteRucolaRepository } from './SQLiteRucolaRepository';
 
 let repositoryPromise: Promise<SQLiteRucolaRepository> | null = null;
@@ -6,7 +7,12 @@ let repositoryPromise: Promise<SQLiteRucolaRepository> | null = null;
 export function getRepository(): Promise<SQLiteRucolaRepository> {
   if (!repositoryPromise) {
     repositoryPromise = initializeDatabase()
-      .then((db) => new SQLiteRucolaRepository(db))
+      .then(async (db) => {
+        const repository = new SQLiteRucolaRepository(db);
+        const messages = await repository.getMessages();
+        await reconcileOwnedMedia(messages.flatMap((message) => message.mediaReference ? [message.mediaReference] : []));
+        return repository;
+      })
       .catch((cause) => {
         repositoryPromise = null;
         throw cause;
